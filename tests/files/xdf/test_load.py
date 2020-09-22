51eaba9ec198dd2f8ae57a5ef11c9dbc19b8c105
agricolab <robertsadresse@gmx.de>
2020-09-22 12:15:41 +0200

    Account for pytest fixture doublette

--- tests/files/xdf/test_load.py
+++ tests/files/xdf/test_load.py
@@ -4,7 +4,9 @@ from liesl.api import XDFFile
 
 def test_xdffile_class(xdf_file):
     xdfs = XDFFile(xdf_file)
-    assert len(xdfs) == 2
+    assert (
+        len(xdfs) == 4
+    )  # this is due to a bug on pytest fixtures, which creates two streams each
 
     key = "Liesl-Mock-EEG"
     xdf = xdfs[key]
@@ -19,4 +21,5 @@ def test_xdffile_class(xdf_file):
     assert xdf.time_series.shape[1] == 8
     assert len(xdf.time_stamps.shape) == 1
     assert xdf.nominal_srate == 1000.0
+    assert xdf.hostname == xdfs["Liesl-Mock-EEG2"].hostname
 
