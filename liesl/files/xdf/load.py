64d5e2532eede51eeabb81b30092e103bd67a3a8
agricolab <robertsadresse@gmx.de>
2020-09-28 10:56:19 +0200

    Handle repr for XDFstreams with no event

--- liesl/files/xdf/load.py
+++ liesl/files/xdf/load.py
@@ -122,7 +122,10 @@ class XDFStream:
         return self._stream["time_stamps"]
 
     def __repr__(self) -> str:
-        return f"XDFStream(name={self.name}, type={self.type}, hostname={self.hostname}, created_at={self.created_at}, duration={self.time_stamps[0]-self.time_stamps[1]}, chan_count={self.channel_count}, )"
+        try:
+            return f"XDFStream(name={self.name}, type={self.type}, hostname={self.hostname}, created_at={self.created_at}, duration={self.time_stamps[0]-self.time_stamps[1]}, chan_count={self.channel_count}, )"
+        except IndexError:
+            return f"XDFStream(name={self.name}, type={self.type}, hostname={self.hostname}, created_at={self.created_at}, chan_count={self.channel_count}, )"
 
 
 # -----------------------------------------------------------------------------
